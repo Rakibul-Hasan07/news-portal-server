e7b65f1b98a523108c9d6bee4a28bf86408184dc
shihab00027 <shihabhasan981@gmail.com>
2022-10-22 14:29:36 +0600

    some update

--- index.js
+++ index.js
@@ -14,8 +14,20 @@ app.get('/category', (req, res) => {
     res.send(category)
 })
 
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const category_news = news.filter(n => n.category_id === id)
+    res.send(category_news)
+})
+
 app.get('/news', (req, res) => {
     res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const AllNews = news.find(n => n._id === id)
+    res.send(AllNews) 
 })
 
 app.listen(port, () => {
